94b9cc9e93e9bd12b6b6cebb949fec495cdf43c2
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-12-25 20:09:41 +0100

    Fix symbol not being used on coinmarketcap

--- src/exchange/implementation/coinmarketcap.rs
+++ src/exchange/implementation/coinmarketcap.rs
@@ -17,14 +17,14 @@ impl Api for CoinMarketCap {
     }
 
     fn ticker(&self, pair: (Coin, Coin)) -> Result<TradingTicker> {
-        let coins = Self::parse_coins(pair);
+        let coins = Self::parse_coins(&pair);
         let endpoint = format!("ticker/{}/?convert={}", coins.0, coins.1);
         let response = self.make_request(&endpoint).send()?.text()?;
         let response: Value = serde_json::from_str(&response)?;
         let response = &response[0];
 
         let daily_change_percentage = parse_field(&response["percent_change_24h"])?;
-        let field = format!("price_{}", coins.1);
+        let field = format!("price_{}", Self::parse_symbol(&pair.1));
         let last_trade_price = parse_field(&response[&field])?;
 
         Ok(TradingTicker {
@@ -39,18 +39,29 @@ impl Api for CoinMarketCap {
 }
 
 impl CoinMarketCap {
-    fn parse_coins(coins: (Coin, Coin)) -> (String, String) {
-        (Self::parse_coin(coins.0), Self::parse_coin(coins.1))
+    fn parse_coins(coins: &(Coin, Coin)) -> (String, String) {
+        (Self::parse_coin(&coins.0), Self::parse_coin(&coins.1))
     }
 
-    fn parse_coin(coin: Coin) -> String {
-        match coin {
+    fn parse_coin(coin: &Coin) -> String {
+        match *coin {
             Coin::USDollar => "usd",
             Coin::Bitcoin => "bitcoin",
             Coin::Ethereum => "ethereum",
             Coin::Iota => "iota",
 
             Coin::RequestNetwork => "request-network"
+        }.to_string()
+    }
+
+    fn parse_symbol(coin: &Coin) -> String {
+        match *coin {
+            Coin::USDollar => "usd",
+            Coin::Bitcoin => "btc",
+            Coin::Ethereum => "eth",
+            Coin::Iota => "miota",
+
+            Coin::RequestNetwork => "req"
         }.to_string()
     }
 
