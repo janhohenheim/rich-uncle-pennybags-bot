2c9fed2aa73a2141bb692cf260a752b19e8b8a2b
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-12-26 21:35:11 +0100

    Improve install script by not sending stuff twice over the wire

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-LOCATION=/opt/rich-uncle-pennybags-bot &&
+LOCATION=/opt/rich-uncle-pennybags-bot/ &&
 echo  -e "\033[33;36m Updating source... \033[0m" &&
 ssh jnf "(cd rich-uncle-pennybags-bot && git pull)" &&
 scp .env jnf:~/rich-uncle-pennybags-bot/ &&
-scp coins.toml jnf:$LOCATION &&
 
 echo  -e "\033[33;36m Building release binaries... \033[0m" &&
 ssh jnf "(cd rich-uncle-pennybags-bot && /root/.cargo/bin/cargo build --release)" &&
@@ -13,6 +12,7 @@ ssh jnf service rich-uncle-pennybags-bot stop &&
 
 echo  -e "\033[33;36m Copying files to $LOCATION... \033[0m" &&
 ssh jnf cp rich-uncle-pennybags-bot/target/release/rich-uncle-pennybags-bot $LOCATION &&
+ssh jnf cp rich-uncle-pennybags-bot/coins.toml $LOCATION &&
 
 echo  -e "\033[33;36m Starting services... \033[0m" &&
 ssh jnf service rich-uncle-pennybags-bot start &&
