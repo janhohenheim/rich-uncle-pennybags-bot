ca65d196b231baf2b530825ede4b6427c6e6235c
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-12-26 21:38:36 +0100

    Make install script DRYer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-LOCATION=/opt/rich-uncle-pennybags-bot/ &&
+BOTNAME=rich-uncle-pennybags-bot &&
+LOCATION=/opt/$BOTNAME/ &&
 echo  -e "\033[33;36m Updating source... \033[0m" &&
-ssh jnf "(cd rich-uncle-pennybags-bot && git pull)" &&
-scp .env jnf:~/rich-uncle-pennybags-bot/ &&
+ssh jnf "(cd $BOTNAME && git pull)" &&
+scp .env jnf:~/$BOTNAME/ &&
 
 echo  -e "\033[33;36m Building release binaries... \033[0m" &&
-ssh jnf "(cd rich-uncle-pennybags-bot && /root/.cargo/bin/cargo build --release)" &&
+ssh jnf "(cd $BOTNAME && /root/.cargo/bin/cargo build --release)" &&
 
 echo  -e "\033[33;36m Stopping services... \033[0m" &&
-ssh jnf service rich-uncle-pennybags-bot stop &&
+ssh jnf service $BOTNAME stop &&
 
 echo  -e "\033[33;36m Copying files to $LOCATION... \033[0m" &&
-ssh jnf cp rich-uncle-pennybags-bot/target/release/rich-uncle-pennybags-bot $LOCATION &&
-ssh jnf cp rich-uncle-pennybags-bot/coins.toml $LOCATION &&
+ssh jnf cp $BOTNAME/target/release/$BOTNAME $LOCATION &&
+ssh jnf cp $BOTNAME/coins.toml $LOCATION &&
 
 echo  -e "\033[33;36m Starting services... \033[0m" &&
-ssh jnf service rich-uncle-pennybags-bot start &&
+ssh jnf service $BOTNAME start &&
 
 echo  -e "\033[33;32m Done deploying! \033[0m";
