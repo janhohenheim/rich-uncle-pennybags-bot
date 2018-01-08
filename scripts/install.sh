aaf3023cdfac1cbf116f167ac36cad386c8f93dc
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-01-08 10:32:31 +0100

    Actually remove .env copying. It has the right to be different on every platform anyways

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@ BOTNAME=rich-uncle-pennybags-bot &&
 LOCATION=/opt/$BOTNAME/ &&
 echo  -e "\033[33;36m Updating source... \033[0m" &&
 ssh jnf "(cd $BOTNAME && git pull)" &&
-scp .env jnf:~/$BOTNAME/ &&
 
 echo  -e "\033[33;36m Building release binaries... \033[0m" &&
 ssh jnf "(cd $BOTNAME && /root/.cargo/bin/cargo build --release)" &&
@@ -13,7 +12,6 @@ ssh jnf service $BOTNAME stop &&
 
 echo  -e "\033[33;36m Copying files to $LOCATION... \033[0m" &&
 ssh jnf cp $BOTNAME/target/release/$BOTNAME $LOCATION &&
-ssh jnf cp $BOTNAME/.env $LOCATION &&
 ssh jnf cp $BOTNAME/Coins.toml $LOCATION &&
 
 echo  -e "\033[33;36m Starting services... \033[0m" &&
