87d1e800c05c5548c83ebd08623667bf61b43e67
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-01-08 14:16:13 +0100

    Replace scp with rsync for them speed gainz

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,10 @@ echo  -e "\033[33;36m Building release binaries... \033[0m" &&
 cargo build --release &&
 
 echo  -e "\033[33;36m Copying binaries to server... \033[0m" &&
-ssh jnf rm -rf ~/$BOTNAME/target/release/$BOTNAME &&
-scp ./target/release/$BOTNAME jnf:~/$BOTNAME/target/release/$BOTNAME &&
+rsync -av ./target/release/$BOTNAME jnf:~/$BOTNAME/target/release/$BOTNAME &&
 
 echo  -e "\033[33;36m Copying Coinfile to server... \033[0m" &&
-ssh jnf rm -rf ~/$BOTNAME/Coins.toml &&
-scp ./Coins.toml jnf:~/$BOTNAME/Coins.toml &&
+rsync -av  ./Coins.toml jnf:~/$BOTNAME/Coins.toml &&
 
 echo  -e "\033[33;36m Stopping services... \033[0m" &&
 ssh jnf service $BOTNAME stop &&
